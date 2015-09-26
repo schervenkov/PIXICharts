9fbfc54685a59a7232204684461e1f4a0eeb6247
chalgoman <chalgoman@gmail.com>
2015-09-26 20:31:39 +0300

    changes

--- src/BarChart.js
+++ src/BarChart.js
@@ -10,7 +10,9 @@
 			showLabels: true,
 			showValues: true,
 			backgroundColor: null,
-			offset: 10,
+			barsOffset: 10,
+			leftOffset: 30,
+			bottomOffset: 20,
 			chartTitle: null,
 			font: 'Arial',
 			valueFormat: function(val) { return val; }
@@ -23,13 +25,16 @@
 		// getColor util
 		this.getColor = charts.colors();
 
+		this.tempChildren = [];
+
 		// background element
 		this.background = this.addChild( new PIXI.Graphics() );
 		this.setBackgroundColor( this.config.backgroundColor );
 
 		// create chart container
 		this.chartContainer = this.addChild(new PIXI.Container());
-		this.chart = this.chartContainer.addChild(new PIXI.Graphics);
+		this.chart = this.chartContainer.addChild(new PIXI.Graphics());
+		this.chartLines = this.chartContainer.addChild(new PIXI.Graphics());
 	};
 
 	BarChart.prototype = Object.create( PIXI.Container.prototype );
@@ -37,10 +42,10 @@
 	charts.extend(BarChart.prototype, {
 		_drawSector: function( xPos, maxValue, key ) {
 			var totalSectors = Object.keys(this.data).length;
-			var sectorWidth = (this.config.width - (totalSectors-1)*this.config.offset) / totalSectors;
+			var sectorWidth = (this.config.width - (totalSectors-1) * this.config.barsOffset - this.config.leftOffset) / totalSectors;
 
-			var sectorHeight = this.config.height * (this.data[key] / maxValue);
-			var yPos = this.config.height - sectorHeight;
+			var sectorHeight = this.config.height * (this.data[key] / maxValue) - this.config.bottomOffset;
+			var yPos = this.config.height - sectorHeight - this.config.bottomOffset;
 
 			this.chart
 					.beginFill(this.colors[key])
@@ -52,12 +57,76 @@
 				y: yPos + (sectorHeight / 2)
 			});
 
-			return xPos + sectorWidth + this.config.offset;
+			return xPos + sectorWidth + this.config.barsOffset;
 		},
 
 		// draws the value (if needed) at given point
 		_drawValue: function( key, position ) {
 			if (!this.config.showValues && !this.config.showLabels) return;
+
+			var textParts = [];
+			if (this.config.showLabels) {
+				textParts.push( this.labels[key] ? this.labels[key] : key );
+			}
+			if (this.config.showValues) {
+				textParts.push( this.config.valueFormat(this.data[key]) );
+			}
+
+			var text = new PIXI.Text( textParts.join(' '), {
+				font: '12px ' + this.config.font,
+				fill: 0xFFFFFF
+			});
+
+			var bgPadding = 5;
+
+			var background = new PIXI.Graphics()
+										.beginFill(0x000000, 0.5)
+										.drawRoundedRect(0, 0, text.width + bgPadding * 2, text.height + bgPadding * 2, 5)
+										.endFill();
+
+			this.tempChildren.push(this.chartContainer.addChild(background));
+			this.tempChildren.push(this.chartContainer.addChild(text));
+
+
+			position.x = Math.round(position.x);
+			position.y = Math.round(position.y);
+
+			text.anchor.set(0.5, 0.5);
+			background.position.set(position.x - text.width / 2 - bgPadding, position.y - text.height / 2 - bgPadding);
+			text.position.set(position.x, position.y);
+		},
+
+		_drawLines: function( minValue, maxValue ) {
+			this.chartLines
+					.lineStyle(1, 0x000000)
+					.moveTo(this.config.leftOffset, 0)
+					.lineTo(this.config.leftOffset, this.config.height)
+					.moveTo(0, this.config.height - this.config.bottomOffset)
+					.lineTo(this.config.width, this.config.height - this.config.bottomOffset);
+
+			// 20px per sector
+			var sectorHeight = 20;
+			var sectors = (this.config.height - this.config.bottomOffset) / sectorHeight;
+			var valuePerSector = Math.round(maxValue / sectors);
+
+			for (var sectorVal = 0, i = 0; sectorVal <= maxValue; sectorVal += valuePerSector) {
+				var y = this.config.height - (i * sectorHeight + this.config.bottomOffset);
+				this.chartLines
+						.lineStyle(1, 0x000000)
+						.moveTo( this.config.leftOffset - 5, y )
+						.lineTo( this.config.leftOffset + 5, y );
+
+				if (sectorVal > 0) {
+					var text = this.chartLines.parent.addChild(new PIXI.Text( this.config.valueFormat(sectorVal), {
+						font: '10px ' + this.config.font,
+						fill: 0x000000
+					}));
+					text.anchor.set(0, 0.5);
+					text.position.set(0, y);
+				}
+
+				i++;
+			}
 		},
 
 		setBackgroundColor: function( color ) {
@@ -101,14 +170,30 @@
 			return this;
 		},
 
+		clear: function() {
+			this.tempChildren.forEach(function(child) {
+				if (child.parent) {
+					child.parent.removeChild(child);
+				}
+			});
+			this.tempChildren = [];
+			this.chart.clear();
+		},
+
 		redraw: function() {
-			var nextSectorX = 0;
-			var maxValue = 0;
+			this.clear();
+
+			var nextSectorX = this.config.leftOffset;
+			var maxValue = null,
+				minValue = null;
 
 			for (var key in this.data) {
 				if ( !this.data.hasOwnProperty(key) ) continue;
-				if ( this.data[key] > maxValue ) {
+				if ( maxValue === null || this.data[key] > maxValue ) {
 					maxValue = this.data[key];
+				}
+				if ( minValue === null || this.data[key] < minValue ) {
+					minValue = this.data[key];
 				}
 			}
 
@@ -119,6 +204,8 @@
 				}
 				nextSectorX = this._drawSector(nextSectorX, maxValue, key);
 			}
+
+			this._drawLines(minValue, maxValue);
 		}
 	});
 
